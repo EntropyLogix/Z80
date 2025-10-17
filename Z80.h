c907f0aaf15941be10aa4d8f6cd8d21e688674e3
Adam Szulc <aszulc@gmail.com>
2025-10-17 15:01:15 +0200

    Adding bit_8bit_HL_ptr.

--- Z80.h
+++ Z80.h
@@ -938,6 +938,18 @@ private:
             .update(Flags::S, bit == 7 && !bit_is_zero);
         set_F(flags);
     }
+    void bit_8bit_HL_ptr(uint8_t bit, uint8_t value) {
+        bool bit_is_zero = (value & (1U << bit)) == 0;
+        Flags flags = get_F();
+        flags.set(Flags::H)
+            .clear(Flags::N)
+            .update(Flags::Z, bit_is_zero)
+            .update(Flags::PV, bit_is_zero)
+            .update(Flags::S, bit == 7 && !bit_is_zero)
+            .update(Flags::X, (get_W() & Flags::X) != 0)
+            .update(Flags::Y, (get_W() & Flags::Y) != 0);
+        set_F(flags);
+    }
     uint8_t res_8bit(uint8_t bit, uint8_t value) {
         return value & ~(1 << bit);
     }
@@ -1082,18 +1094,11 @@ private:
                 }
                 break;
             case 1: {
-                bit_8bit(bit, value);
-                Flags flags = get_F();
                 if (target_reg == 6) {
-                    set_WZ(flags_source);
+                    bit_8bit_HL_ptr(bit, value);
                     add_tick();
-                    flags.update(Flags::X, (get_W() & Flags::X) != 0);
-                    flags.update(Flags::Y, (get_W() & Flags::Y) != 0);
-                } else {
-                    flags.update(Flags::X, (value & Flags::X) != 0);
-                    flags.update(Flags::Y, (value & Flags::Y) != 0);
-                }
-                set_F(flags);
+                } else
+                    bit_8bit(bit, value);
                 return;
             }
             case 2: result = res_8bit(bit, value); break;
