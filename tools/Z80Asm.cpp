d44cab2552178bf3e923b2ccb801e49193c6754e
Adam Szulc <aszulc@gmail.com>
2025-11-07 23:41:44 +0100

    INCBIN fix.

--- tools/Z80Asm.cpp
+++ tools/Z80Asm.cpp
@@ -35,21 +35,22 @@ void print_usage() {
 
 class FileSystemSourceProvider : public ISourceProvider {
 public:
-    bool get_source(const std::string& identifier, std::string& source) override {
+    bool get_source(const std::string& identifier, std::vector<uint8_t>& data) override {
         std::filesystem::path file_path;
         if (m_current_path_stack.empty())
             file_path = std::filesystem::canonical(identifier);
         else
             file_path = std::filesystem::canonical(m_current_path_stack.back().parent_path() / identifier);
         m_current_path_stack.push_back(file_path);
-        std::ifstream file(file_path);
+        std::ifstream file(file_path, std::ios::binary | std::ios::ate);
         if (!file) {
             m_current_path_stack.pop_back();
             return false;
         }
-        std::stringstream buffer;
-        buffer << file.rdbuf();
-        source = buffer.str();
+        std::streamsize size = file.tellg();
+        file.seekg(0, std::ios::beg);
+        data.resize(size);
+        file.read(reinterpret_cast<char*>(data.data()), size);
         m_current_path_stack.pop_back();
         return true;
     }
